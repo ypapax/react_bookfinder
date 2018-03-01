2ecfbf55488abcdefdce73a6bedac747b0825aab
Maxim Yefremov <maxYefr@gmail.com>
2018-03-01 13:21:24 +0300

    components outline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from "./Components/Header";
+import {Grid, Col, Row} from 'react-bootstrap'
+
+import Books from './Components/Books'
 
 class App extends Component {
+  constructor(){
+      super();
+      this.state = {
+          books: [],
+          text: 'Harry Potter'
+      }
+  }
+
   render() {
     return (
       <div className="App">
-        App
+        <Header/>
+          <Grid>
+              <Row>
+                  <Col xs={12} md={12} lg={12}>
+                      <Books/>
+                  </Col>
+              </Row>
+          </Grid>
       </div>
     );
   }
---
+++ src/Components/Books.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Navbar} from 'react-bootstrap';
+
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                BOOKS
+            </div>
+        );
+    }
+}
+
+export default Header;
