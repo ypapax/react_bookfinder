91790be6b0915f866f4e049a1ed86da8c5095bfb
Maxim Yefremov <maxYefr@gmail.com>
2018-03-01 14:54:36 +0300

    book panel group

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ class App extends Component {
           <Grid>
               <Row>
                   <Col xs={12} md={12} lg={12}>
-                      <Books/>
+                      <Books books={this.state.books}/>
                   </Col>
               </Row>
           </Grid>
--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap';
+import {Grid, Row, Col, Accordion, Panel} from 'react-bootstrap';
+import PanelGroup from "react-bootstrap/es/PanelGroup";
 
 
 class Header extends Component {
     render() {
+        let bookItems;
+        if (this.props.books) {
+            bookItems = this.props.books.map(book => {
+                let id = book.id;
+                let title = book.volumeInfo.title;
+                return (
+                    <Panel eventKey={id} key={id}>
+                        <Panel.Heading>
+                            <Panel.Title toggle>{title}</Panel.Title>
+                        </Panel.Heading>
+                        <Panel.Body collapsible>
+                            test
+                        </Panel.Body>
+                    </Panel>
+                )
+            });
+        }
         return (
             <div>
-                BOOKS
+                <PanelGroup accordion id="books-panel-group">
+                    {bookItems}
+                </PanelGroup>
             </div>
         );
     }
