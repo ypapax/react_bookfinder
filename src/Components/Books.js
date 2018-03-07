443ff359912a9ad6e2318b62f54f7b2727daf71c
Maxim Yefremov <maxYefr@gmail.com>
2018-03-07 12:43:15 +0300

    show book info in accordion

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import {Grid, Row, Col, Accordion, Panel} from 'react-bootstrap';
-
+import {Grid, Row, Col, Accordion, Panel, ListGroup, ListGroupItem, Button} from 'react-bootstrap';
 
 class Header extends Component {
     render() {
@@ -9,10 +8,43 @@ class Header extends Component {
             bookItems = this.props.books.map(book => {
                 let id = book.id;
                 let title = book.volumeInfo.title;
+                let thumbnail = book.volumeInfo.imageLinks.thumbnail;
+                let categories = book.volumeInfo.categories;
+                let authors = book.volumeInfo.authors;
+                let publisher = book.volumeInfo.publisher;
+                let description = book.volumeInfo.description;
+                let pageCount = book.volumeInfo.pageCount;
+                let publishDate = book.volumeInfo.publishDate;
+                let averageRating = book.volumeInfo.averageRating;
+                let buyLink = book.volumeInfo.infoLink;
+                console.info("buyLink", buyLink);
                 return (
                     <Panel header={title} eventKey={id} key={id}>
-                            test
-
+                        <Grid>
+                            <Row>
+                                <Col xs={3} md={3} lg={3}>
+                                    <img src={thumbnail} role="presentation"/>
+                                </Col>
+                                <Col xs={8} md={8} lg={8}>
+                                    <ListGroup>
+                                        <ListGroupItem><strong>Categories:</strong> {categories}</ListGroupItem>
+                                        <ListGroupItem><strong>Authors:</strong> {authors}</ListGroupItem>
+                                        <ListGroupItem><strong>Publisher:</strong> {publisher}</ListGroupItem>
+                                        <ListGroupItem><strong>Publishe Date:</strong> {publishDate}</ListGroupItem>
+                                        <ListGroupItem><strong>Page Count:</strong> {pageCount}</ListGroupItem>
+                                        <ListGroupItem><strong>Average Rating:</strong> <span className="rating">{averageRating}</span></ListGroupItem>
+                                    </ListGroup>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={11} md={11} lg={11}>
+                                    <h3>Book Description</h3>
+                                    {description}
+                                    <hr/>
+                                    <Button href={buyLink} bsStyle="primary">Buy Now</Button>
+                                </Col>
+                            </Row>
+                        </Grid>
                     </Panel>
                 )
             });
